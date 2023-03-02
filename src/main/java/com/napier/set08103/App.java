9479cdb31bf5dd84e06c1f95b8d347ee56bd6c55
Finlay Daniel Coull <58175445+fdcoull@users.noreply.github.com>
2023-03-02 17:48:57 +0000

    Update JDK version

--- src/main/java/com/napier/set08103/App.java
+++ src/main/java/com/napier/set08103/App.java
@@ -1,6 +1,10 @@
 package com.napier.set08103;
 
 public class App {
+    /**
+     * hello
+     * @param args Hello
+     */
     public static void main(String[] args) {
         System.out.println("Test");
     }
