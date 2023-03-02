ae723f6c28dce0fdff713211bfdfc338c98db2a5
Finlay Daniel Coull <58175445+fdcoull@users.noreply.github.com>
2023-03-02 19:47:09 +0000

    Update MongoDB hostname

--- src/main/java/com/napier/set08103/App.java
+++ src/main/java/com/napier/set08103/App.java
@@ -8,7 +8,7 @@ import org.bson.Document;
 public class App {
     public static void main(String[] args) {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        MongoClient mongoClient = new MongoClient("set08103_assessment");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
