d5be82a5eb3ef5819a708bafab8bcf8fb394807b
Finlay Daniel Coull <58175445+fdcoull@users.noreply.github.com>
2023-04-13 18:47:52 +0100

    Bug fix for app not updating 1

--- src/main/java/com/napier/set08103/App.java
+++ src/main/java/com/napier/set08103/App.java
@@ -27,7 +27,7 @@ public class App {
         int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
-            System.out.println("Connecting to database...");
+            System.out.println("Connecting to database...TEST");
             try
             {
                 // Wait a bit for db to start
@@ -35,6 +35,7 @@ public class App {
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
+                System.out.println("Test point 2");
                 break;
             }
             catch (SQLException sqle)
@@ -68,12 +69,68 @@ public class App {
         }
     }
 
-    public static void main(String[] args) {
+    public City getCity(int ID) throws SQLException {
+        try
+        {
+            //Create an sql statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT ID, Name "
+                            + "FROM City "
+                            + "WHERE ID = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+
+            if (rset.next()) {
+                City city = new City();
+                city.ID = rset.getInt("ID");
+                city.Name = rset.getString("Name");
+                return city;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+    public void displayCity(City city)
+    {
+        if (city != null)
+        {
+            System.out.println(
+                    "City: "
+                    + city.ID + " "
+                    + city.Name
+            );
+        }
+        else
+        {
+            System.out.println("No city found");
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
+
+        // Get city
+        City city = a.getCity(1);
+
+        // Display results
+        a.displayCity(city);
+
+        System.out.println("Test point 1");
 
         // Disconnect from database
         a.disconnect();
---
+++ src/main/java/com/napier/set08103/City.java
@@ -0,0 +1,38 @@
+package com.napier.set08103;
+
+import java.sql.*;
+
+/**
+ * Represents a city
+ */
+public class City {
+    /**
+     * ID
+     */
+    public int ID;
+
+    /**
+     * Name
+     */
+    public String Name;
+
+    /**
+     * Country Code
+     */
+    public String CountryCode;
+
+    /**
+     * District
+     */
+    public String District;
+
+    /**
+     * Population
+     */
+    public int Population;
+
+
+
+}
+
+
