fc05a09915bf14b7c5580899c4f134d137ccecc6
Finlay Daniel Coull <58175445+fdcoull@users.noreply.github.com>
2023-04-13 14:28:36 +0100

    Add connect and disconnect methods

--- src/main/java/com/napier/set08103/App.java
+++ src/main/java/com/napier/set08103/App.java
@@ -4,10 +4,15 @@ import java.sql.*;
 
 public class App {
     /**
-     * hello
-     * @param args Hello
+     * Connection to MySQL database.
      */
-    public static void main(String[] args) {
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
         try
         {
             // Load Database driver
@@ -19,9 +24,7 @@ public class App {
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -32,9 +35,6 @@ public class App {
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -47,7 +47,13 @@ public class App {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -60,5 +66,16 @@ public class App {
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
